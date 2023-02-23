3c9903b257072db84f3efefb4f128d5e4b4e54cb
Matej Badin <matej.badin@gmail.com>
2023-02-23 10:52:44 +0100

    Second Exercise 27/02/2022

---
+++ EX01-relax/N2/plotdist/plot.dist.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os
+import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
+from matplotlib import pyplot as plt
+
+results = []
+
+for d, dn, fn in os.walk("."):
+    for dir in sorted(dn):
+        if "scf-" in dir:
+            try:
+                E = 0
+                with open(d+"/"+dir+"/scf.out") as f:
+                    for line in f:
+                        if "!" in line:
+                             E = float(line.split()[4])
+                results.append([float(dir[4:]), E])
+            except:
+                pass
+
+results = sorted(results, key=lambda x: x[0])
+results = np.array(results).T
+plt.plot(results[0],results[1], linewidth=2)
+plt.savefig("plot.png")
